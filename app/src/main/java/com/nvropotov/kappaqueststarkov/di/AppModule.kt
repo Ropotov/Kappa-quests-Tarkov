b7b33fb713fd53f14d9a28fd99392827594a4b52
Nikita Ropotov <ropotov.dev@gmail.com>
2025-09-23 20:21:35 +0300

    add QuestApi.kt

--- app/src/main/java/com/nvropotov/kappaqueststarkov/di/AppModule.kt
+++ app/src/main/java/com/nvropotov/kappaqueststarkov/di/AppModule.kt
@@ -2,10 +2,17 @@ package com.nvropotov.kappaqueststarkov.di
 
 import android.app.Application
 import com.nvropotov.kappaqueststarkov.data.database.QuestDatabase.Companion.getMainDatabase
+import com.nvropotov.kappaqueststarkov.data.network.QuestApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://raw.githubusercontent.com/"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,4 +21,30 @@ class AppModule {
     @Provides
     fun provideQuestDao(application: Application) =
         getMainDatabase(application.applicationContext).dao()
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideQuestApi(
+        okHttpClient: OkHttpClient
+    ): QuestApi =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestApi::class.java)
+
 }
